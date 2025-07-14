6320c01c84bb9c998bc9460b3290d27f5ec0f360
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2025-07-14 11:01:43 +0200

    Update hot reload configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
 }
 plugins {
     //https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
-    id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
 include(":composeApp")
